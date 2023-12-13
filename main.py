a50439a13fbfe12b76c16fd686d159f0671b2d75
NikolaiJamez <nikolaijamez@gmail.com>
2023-12-14 09:38:56 +1300

    Basic Appbar configured

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 import flet as ft
 
 def main(page: ft.Page) -> None:
-    def check_item_clicked(e):
-        e.control.checked = not e.control.checked
-        page.update()
+    def close_app(e: ft.ControlEvent) -> None:
+        page.window_close()
+
+    def run_rules(e: ft.ControlEvent) -> None:
+        raise NotImplementedError('Running rules has not been implemented yet!')
 
     page.window_height = 600
     page.window_width = 1000
     page.window_resizable = False
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    last_run_value = 'Never'
+    last_run_control = ft.Text(last_run_value)
 
     page.appbar = ft.AppBar(
-        leading = ft.Icon(ft.icons.PALETTE),
+        leading = ft.Icon(ft.icons.DOWNLOADING_ROUNDED),
         leading_width = 40,
-        title = ft.Text("AppBar Example"),
+        title = ft.Text('Downloads Manager'),
         center_title = False,
         bgcolor = ft.colors.SURFACE_VARIANT,
         actions = [
-            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
-            ft.IconButton(ft.icons.FILTER_3),
-            ft.PopupMenuButton(
-                items = [
-                    ft.PopupMenuItem(text = "Item 1"),
-                    ft.PopupMenuItem(),  # divider
-                    ft.PopupMenuItem(
-                        text = "Checked item",
-                        checked = False,
-                        on_click = check_item_clicked
-                    ),
-                ]
+            ft.Text('Last Run: '),
+            last_run_control,
+            ft.IconButton(
+                icon = ft.icons.PLAY_ARROW_ROUNDED,
+                icon_color = ft.colors.GREEN,
+                tooltip = 'Run Now',
+                on_click = run_rules,
             ),
+            ft.IconButton(
+                icon = ft.icons.CLOSE,
+                icon_color = ft.colors.RED,
+                tooltip = 'Close App',
+                on_click = close_app,
+            ),
+            ft.VerticalDivider(),
         ],
     )
     page.update()
 
-ft.app(target = main)
+if __name__ == '__main__':
+    ft.app(target = main)

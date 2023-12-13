a3acd185ad29274afed28760d8644732566784a1
NikolaiJamez <nikolaijamez@gmail.com>
2023-12-14 09:25:30 +1300

    Added page constraints

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flet as ft
 
 def main(page: ft.Page) -> None:
-    pass
+    page.window_height = 600
+    page.window_width = 1000
+    page.window_resizable = False
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
 ft.app(target = main)
